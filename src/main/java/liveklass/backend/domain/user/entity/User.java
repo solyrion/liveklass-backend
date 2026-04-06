03e8c7e2dbba33a0a9a3d2b252e96d7df5d7f174
seodong-eun <sde101573@gmail.com>
2026-04-06 23:59:15 +0900

    feat: add User entity

---
+++ src/main/java/liveklass/backend/domain/user/entity/User.java
@@ -0,0 +1,40 @@
+package liveklass.backend.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public User(String name, String email, UserRole role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.createdAt = LocalDateTime.now();
+    }
+}
